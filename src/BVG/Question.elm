acda6347b54a65537a751981d1c86cf3ebc0cfd5
Marcelo Boeira <me@marceloboeira.com>
2019-02-10 14:11:52 +0100

    Delete old "question" code

--- src/BVG/Question.elm
+++
@@ -1,8 +0,0 @@
-module BVG.Question exposing (Question)
-
-import BVG.Line as Line exposing (Line)
-import BVG.Station as Station exposing (Station)
-
-
-type alias Question =
-    { station : Station }
