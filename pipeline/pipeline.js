8d753671ab049cbbc605a6d7ef8206cc9949fa90
Marcelo Boeira <me@marceloboeira.com>
2019-02-11 16:23:12 +0100

    Make the pipeline more functional
    Reduce mutability and create more functions rather than imperative code.
    
    The code is now more readable and easier to extend.

--- pipeline/pipeline.js
+++ pipeline/pipeline.js
@@ -6,68 +6,76 @@
 //
 // Refactor
 // Receive options as parameters (output path)
-// Make the pipeline more functional
-const vbbStationPhotos = require("vbb-station-photos");
-const vbbLinesAt = require("vbb-lines-at");
-const vbbStations = require("vbb-stations/full.json");
-const fileSystem = require("fs");
-
-// Globals
-var finalStations = {}
-var finalLines = {}
+const vbbStations = require("vbb-stations/full.json")
+const vbbLinesAt = require("vbb-lines-at")
+const vbbStationPhotos = require("vbb-station-photos")
+const fileSystem = require("fs")
+const md5 = require("md5")
 
 const hashToArray = (hash) => {
   return Object.keys(hash).reduce((result, id) => {
     return result.concat(hash[id])
-  }, []);
+  }, [])
 }
 
 const toJSON = (input) => JSON.stringify(input, null, 2)
 
 const toFile = (name, content) => {
   return fileSystem.writeFile(name, content, (err) => {
-    if (err) console.log(err);
+    if (err) console.log(err)
 
-    console.log(name, "generated!");
-  });
+    console.log(name, "generated!")
+  })
 }
 
-for (var vbbStationId in vbbStations) {
-  var vbbStation = vbbStations[vbbStationId]
-  var vbbStationLines = vbbLinesAt[vbbStationId]
-  var hasSubwayLine = false
+/* Regexp to clean the names
+ * From                            | To
+ * S+U Neukölln (Berlin) [U7]      | Neukölln
+ * U AlexanderPlatz (Berlin) [U8]  | Alexanderplatz
+ * U Bullowstr. (Berlin) [U55]     | Bullowstr.
+ * U Stadmitte U2                  | Stadmitte
+ * S+U Rathaus Steglitz (Bhf) [U9] | Rathaus Steglitz
+ */
+const cleanStationName = (name) => name.replace(/((S\+U)|(U\s)|(\(.*?\))|(\[?U[0-9]*\]?))/g, "").trim()
 
-  var stationLines = []
-  for (var id in vbbStationLines) {
-    var vbbLine = vbbStationLines[id]
-    if (vbbLine["mode"] == "train" && vbbLine["product"] == "subway") {
-      hasSubwayLine = true
-
-      // Add to the unique list of Lines
-      finalLines[vbbLine["id"]] = { "id": vbbLine["id"], "name": vbbLine["name"] }
-
-      stationLines.push(vbbLine["id"])
-    }
-  }
-
-  if (hasSubwayLine) {
-    // Regexp to clean the names
-    // From                            | To
-    // S+U Neukölln (Berlin) [U7]      | Neukölln
-    // U AlexanderPlatz (Berlin) [U8]  | Alexanderplatz
-    // U Bullowstr. (Berlin) [U55]     | Bullowstr.
-    // U Stadmitte U2                  | Stadmitte
-    // S+U Rathaus Steglitz (Bhf) [U9] | Rathaus Steglitz
-
-    cleanStationName = vbbStation["name"].replace(/((S\+U)|(U\s)|(\(.*?\))|(\[?U[0-9]*\]?))/g, "").trim()
-
-    if (finalStations[cleanStationName] == null) {
-      finalStations[cleanStationName] = { "name": cleanStationName, "lines": stationLines}
+const mergeStation = (stations, station, lines) => {
+  const stationName = cleanStationName(station["name"])
+  if (lines.length > 0) {
+    if (stations[stationName] == null) {
+      stations[stationName] = { "name": stationName, "lines": lines }
     } else {
-      finalStations[cleanStationName]["lines"] = stationLines.concat(finalStations[cleanStationName]["lines"])
+      stations[stationName]["lines"] = lines.concat(stations[stationName]["lines"])
     }
   }
+  return stations
 }
 
-toFile("./output/stations.json", toJSON(hashToArray(finalStations)))
-toFile("./output/lines.json", toJSON(hashToArray(finalLines)))
+const mergeLines = (lines, stationLines) => {
+  return stationLines.reduce((lines, line) => {
+    if (lines[line["id"]] == null) {
+      lines[line["id"]] = line
+    }
+
+    return lines
+  }, lines)
+}
+
+const subwayLinesFor = (station) => {
+  return vbbLinesAt[station["id"]]
+    .filter(line => line["product"] == "subway")
+    .map(line => { return { "id": line["id"], "name": line["name"] } })
+}
+
+const state = hashToArray(vbbStations).reduce((state, station) => {
+  var stations = state["stations"]
+  var lines = state["lines"]
+  const stationLines = subwayLinesFor(station)
+
+  return {
+    "stations": mergeStation(stations, station, stationLines),
+    "lines": mergeLines(lines, stationLines)
+  }
+}, { "stations": {}, "lines": {} })
+
+toFile("./output/stations.json", toJSON(hashToArray(state["stations"])))
+toFile("./output/lines.json", toJSON(hashToArray(state["lines"])))
